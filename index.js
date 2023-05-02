b596117c37686fb35e11f3554e65cb3a95eb0ff7
muhiminulalvi <eee1916039brur@gmail.com>
2023-05-02 14:51:32 +0600

    all api done

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.get("/chefs", (req, res) => {
 app.get("/chefs/:id", (req,res) => {
   const id = req.params.id;
   console.log(id);
+  const selectedChef = chefDetails[0].chefs?.find(singleChef => singleChef.id == id);
+  console.log(chefDetails[0].chefs[1].id);
+  res.send(selectedChef)
 })
 
 app.listen(5000, () => {
